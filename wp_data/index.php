27aa110e8ad2b3c564f7d162e5dcc00e286410f7
Darya Kaptsiuh <dasha.kaptyug@yandex.ru>
2022-08-16 13:31:22 +0000

    Revert "edited index.php"
    This reverts commit 6b3b392e23ae2ad5a15565370f6cce24a03669c3.

--- wp_data/index.php
+++ wp_data/index.php
@@ -1,6 +1,4 @@
 <?php
-echo "Test";
-exit;
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
